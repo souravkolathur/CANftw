171e6b6aa581260abaf0b02c3b3314b008a72e97
JERISHAB <jerishabmayan@gmail.com>
2024-04-21 07:26:16 +0700

    LEARNING UPADATED

--- src/pages/Learning/Learning.jsx
+++ src/pages/Learning/Learning.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React from 'react'
+import styles from './Learning.module.css';
 
 function Learning() {
-  return <div>Learning</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={statusbar} />
+        <div className={styles.backarrow}><img src="" alt="" /></div>
+      </div>
+      <div className={styles.head} >
+        <div className={styles.heading}>CH <span className={styles.headTitle}>1</span>|LS <span className={styles.headTitle} >3</span></div>
+        <div className={styles.subhead}>EXPENSE MANAGEMENT</div>
+      </div>
+    </div>
+  )
 }
 
-export default Learning;
+export default Learning
