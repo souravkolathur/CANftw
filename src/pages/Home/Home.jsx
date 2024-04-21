c8b15ae8c8942162848f4624f2db87c2120ca54c
Sourav U P <souravputhanveedu@gmail.com>
2024-04-21 14:13:45 +0530

    Resolved merge conflict

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,16 +4,36 @@ import React, { useEffect } from "react";
 import Navbar from "../../modules/navbar/Navbar";
 import Roadmap from "../Roadmap/Roadmap";
 import { useState } from "react";
+import { supabase } from "../../utils/supa";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const [pageSelect, setPageSelect] = useState("home");
+
+  const [username, setUsername] = useState("Reo");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // get user
+    const getUser = async () => {
+      const { data, error: err } = await supabase.auth.getUser();
+      if (err) {
+        console.log(err);
+      } else {
+        setUsername(data.user.email.split("@")[0]);
+      }
+    };
+
+    getUser();
+  }, []);
   return (
     <div className={styles.outerContainer}>
       {pageSelect == "home" && (
         <div className={styles.container}>
           <div className={styles.heroText}>FINANCIAL QUEST</div>
           <div className={styles.welcome}>
-            WELCOME BACK <span className={styles.baseSpan}>REO</span>{" "}
+            WELCOME BACK{" "}
+            <span className={styles.baseSpan}>{username.toUpperCase()}</span>{" "}
           </div>
           <div className={styles.graph}>
             <div className={styles.percent}>95%</div>
@@ -31,14 +51,24 @@ function Home() {
             <div className={styles.less}> NEXT LESSON &rarr;</div>
             <img src={lessonImg} className={styles.lessonImg} alt="" />
           </div>
+          <button
+            className={styles.logout}
+            onClick={() => {
+              supabase.auth.signOut();
+              navigate("/login");
+            }}
+          >
+            Logout
+          </button>
         </div>
       )}
       {pageSelect == "map" && <Roadmap />}
       {pageSelect == "streak" && <div>Streak</div>}
       {pageSelect == "profile" && <div>Profile</div>}
+
       <Navbar setPageSelect={setPageSelect} />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
